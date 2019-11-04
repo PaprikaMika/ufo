db99e4d5aaf595e981baac7b04961cc5fdea85f1
PaprikaMika <55081913+PaprikaMika@users.noreply.github.com>
2019-11-04 11:28:14 +0100

    Files uploaded

---
+++ app.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const request = require('request');
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+const port = 3000;
+const app = express();
+
+//STEP 1: Setting up the boilerplate and routing
+app.get('/wikipedia', function(req, res) {
+
+  var url = 'https://en.wikipedia.org/wiki/Greek_mythology';
+
+  request(url, function(error, response, html) {
+    if (!error) {
+
+      var $ = cheerio.load(html);
+      var data = {
+        articleTitle: '',
+        articleImg: '',
+        articleParagraph: ''
+
+      }
+      $('#content').filter(function() {
+        data.articleTitle = $(this).find('#firstHeading').text();
+        data.articleImg = $(this).find('img').first().attr('src');
+        data.articleParagraph = $(this).find('p').first().text();
+
+      });
+
+      //All the web scraping magic will happen here
+      res.send(data);
+
+      fs.writeFile('wiki-output.js', JSON.stringify(data, null, 4), function(error) {
+        console.log('file written on hard drive!');
+
+      })
+    }
+  });
+
+});
+
+app.get('/ufo', function(req, res) {
+
+  var url = 'https://www.ufocasebook.com/bestufopictures2.html';
+
+  request(url, function(error, response, html) {
+    if (!error) {
+      var $ = cheerio.load(html);
+
+      var data = []
+
+      $('tbody').filter(function() {
+        $('tr').filter(function() {
+          $('td').filter(function() {
+            $('center').filter(function() {
+              $(this).find('center').each(function(i, elem) {
+                data[i] = "'" + $(this).find('a').find('img').attr('src') + "'";
+              })
+            })
+          })
+        })
+      });
+
+      //All the web scraping magic will happen here
+      res.send(data);
+
+      fs.writeFile('ufo-output.js', 'var ufo_list = [' + data + ']', function(error) {
+        console.log('file written on hard drive!');
+
+      })
+    }
+  });
+
+});
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
+
+exports = module.exports = app;
